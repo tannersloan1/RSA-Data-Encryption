cb1ed7bff1de3f750b3931aa007fdc3b86b5f04f
tannersloan1 <tannersloan05@gmail.com>
2026-03-16 12:03:14 -0500

    No need for random e since it is public

--- key_generator.py
+++ key_generator.py
@@ -1,5 +1,4 @@
 from prime_creation import getPrime
-import random
 
 def GCD(A, B):
     while B != 0:
@@ -8,17 +7,16 @@ def GCD(A, B):
         B = R
     return A
 
-def getE(z):
-    while True:
-        e = random.randrange(2, z)
-        if GCD(e, z) == 1:
-            return e
+e = 65537
 
-p = getPrime()
-q = getPrime()
-n = p*q
-z = (p-1)*(q-1)
-e = getE(z)
+while True:
+    p = getPrime()
+    q = getPrime()
+    n = p*q
+    z = (p-1)*(q-1)
+    if GCD(e, z) == 1:
+        break
+
 d = pow(e, -1, z)
 
 print(f"p value: {p}\nq value: {q}\nn value: {n}\nz value: {z}\ne value: {e}")
