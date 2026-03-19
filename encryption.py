6d66c23089083199d6409618c8cf022e53be9d21
tannersloan1 <tannersloan05@gmail.com>
2026-03-19 00:11:16 -0500

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -9,16 +9,30 @@ currBlock = ""
 while True:
     while currChars != chars:
         char = file.read(1)
+
         # Once EOF reached, pad the rest of the chars to reach 7 chars in the last block then break
         if not char:
             # Need to add padding logic here
             break
-        
-        # Need to add following flow here: ASCII value->Binary value->Concat binary to current 56-bit string
+
+        ASCII = ord(char)
+        binary = format(ASCII, '08b')
+        currBlock += binary
 
         currChars += 1
 
-    # Need to add logic here to encrypt the 56-bit string and either save it or output it before emptying it
+    # Accounts for text size exactly divisible by chars
+    if currBlock == "":
+        break
+
+    decimal = int(currBlock, 2)
+
+    # Hardcoded PK for now
+    e = 65537
+    n = 142330907879694598390734510141721402859
+
+    c = pow(decimal, e, n)
+    print(c) # For test, replace with actualy save/write command
 
     currBlock = ""
     currChars = 0
